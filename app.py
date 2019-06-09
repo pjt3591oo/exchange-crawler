b492bb22cc1b7434640046f20ad62150ec9b35b1
박정태 <bagjeongtae@bagjeongtaeui-MacBookPro.local>
2019-06-09 10:58:17 +0900

    argparser 작업중

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ import argparse
 parser = argparse.ArgumentParser(description='Exchange Crawler CLI Program.', prog="ECC")
 parser.add_argument('--version', action='version', version='%(prog)s Version 1.0 ')
 
+
 def p_s():
-  
+'''
+  upbit
+  coinone
+'''
+
   parser.add_argument('--ticker', type=str, help="cointicker")
   parser.add_argument('--market', type=str, help="market")
+  parser.add_argument('--ped', type=str, help="간격")
+  parser.add_argument('--ped', type=str, help="간격")
   
 
   args = parser.parse_args()
